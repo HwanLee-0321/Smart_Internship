f401092f711b5649faf0d41f5f90fb3392062d11
hwanlee-0321 <ghksdl413@naver.com>
2025-06-26 17:34:05 +0900

    ex04url.js 파일 생성 및 HTTP 서버를 통한 HTML 파일 응답 기능 추가

--- nodejs/JS/ex02http.js
+++ nodejs/JS/ex02http.js
@@ -1,14 +1,10 @@
-const http = require('http'); // node.js core API 모듈중 하나인 http 모듈 사용 하겠다!
+const http = require('http');
 
-// http 모듈 사용해서 서버 열기
-// 새 http 서버 만듦
 http.createServer((req,res)=>{
 
     // 요청 처리..
 
 }).listen(8888, ()=>{
     
-    // 서버가 정상적으로 실행되면 실행하고 싶은 로직
     console.log('good');
 });
-// 8888: 포트번호
---
+++ nodejs/JS/ex04url.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs').promises;
+
+
+http.createServer(async(req,res)=>{
+    const file = await fs.readFile('../html/ex03FS.html');
+
+    // html 파일을 응답
+    // head-> 응답 데이터의 형식, body->응답본문 로 응답
+    // 200(OK) , 400(Bad Request), 404(Not Found)
+    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    res.write(file);
+    res.end();
+
+    const parseURL = url.parse(req.url, true);
+    console.log(parseURL);
+    console.log('pathname: ', parseURL.pathname);
+    console.log('query: ', parseURL.query);
+
+}).listen(8888, ()=>{
+    
+    console.log('Server is running!!');
+});
