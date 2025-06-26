e738d6e92a439a3278798f76fe7ad3498ba0890b
hwanlee-0321 <ghksdl413@naver.com>
2025-06-26 17:10:09 +0900

    비동기 파일 처리 및 HTML 응답 기능 추가

--- nodejs/JS/ex03FS.js
+++ nodejs/JS/ex03FS.js
@@ -1,8 +1,23 @@
 const http = require('http');
+// 파일관련 작업 비동기 처리 
+// 파일시스템 fs 모듈
 
-http.createServer((req,res)=>{
+// async, await 사용
+const fs = require('fs').promises;
 
-    // 요청 처리..
+http.createServer(async (req,res)=>{
+
+    // async/await 사용법
+    // await: 불러와야 하는 파일 앞에 붙이기
+    // html 불러오기
+    const file = await fs.readFile('../html/ex03FS.html');
+
+    // html 파일을 응답
+    // head-> 응답 데이터의 형식, body->응답본문 로 응답
+    // 200(OK) , 400(Bad Request), 404(Not Found)
+    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    res.write(file);
+    res.end();
 
 }).listen(8888, ()=>{
     
