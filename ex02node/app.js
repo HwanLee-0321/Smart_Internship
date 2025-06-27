52a18b2b762aa478c27e94312c9f9a03fbed98f7
hwanlee-0321 <ghksdl413@naver.com>
2025-06-27 16:36:02 +0900

    join 및 login 라우트 수정: 경로 설정 및 응답 파일 추가

--- ex02node/app.js
+++ ex02node/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const app = express();
-const host = "http://localhost:8888";
+const path = require('path'); //경로
 
-app.get('/join', (req, res)=>{
-    // join으로 요청이 들어오면 어떻게 처리할 것인지
-    res.sendFile();
+const app = express(); //app(서버)생성
+
+// localhost:8888/join 요청 => join.html 응답
+app.get('/join', (req,res)=>{
+    ///join으로 요청이 들어오면 어떻게 처리할 것인지
+
+    //절대경로 : ex02express/views/join.html
+    res.sendFile(path.join(__dirname, 'views', 'join.html'));
 });
 
-express(); // app(서버)생성
+// localhost:8888/login
+app.get('/login', (req,res)=>{
+    ///join으로 요청이 들어오면 어떻게 처리할 것인지
+
+    //절대경로 : ex02express/views/join.html
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
 app.listen(8888, ()=>{
-    console.log("Server is running on" + host);
-})
+    console.log('Server is running on http://localhost:8888');
+});
