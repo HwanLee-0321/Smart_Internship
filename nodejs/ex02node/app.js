e1e9538c9e5a1745b99eaa70af306c94f7a619b2
hwanlee-0321 <ghksdl413@naver.com>
2025-07-02 09:16:39 +0900

    라이브러리 설치

--- nodejs/ex02node/app.js
+++ nodejs/ex02node/app.js
@@ -1,44 +1,41 @@
-const express = require('express');
-const path = require('path'); //경로
-const app = express(); //app(서버)생성
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// 바디 Parser
-// 데이터를 가져오기전에 body-Paser가 준비가 되어야지만
-// 제대로 파싱 가능
-app.use(express.urlencoded({extended:true})); // form 태그 작성된 데이터 파싱가능
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// localhost:8888/join 요청 => join.html 응답
-app.get('/join', (req,res)=>{
-    ///join으로 요청이 들어오면 어떻게 처리할 것인지
+var app = express();
 
-    //절대경로 : ex02express/views/join.html
-    res.sendFile(path.join(__dirname, 'views', 'join.html'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// localhost:8888/login
-app.get('/login', (req,res)=>{
-    ///join으로 요청이 들어오면 어떻게 처리할 것인지
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    //절대경로 : ex02express/views/join.html
-    res.sendFile(path.join(__dirname, 'views', 'login.html'));
-});
-// user/join 요청 => 회원가입처리(생략) => login.html응답
-app.post('/user/join', (req, res)=>{
-    // 회원가입처리
-    // 사용자가 입력한 값 확인하기
-    // console.log(req.query); // 요청 데이터 확인 => URL 뒤에 붙은
-    // 쿼리스트링 가져옴
-    console.log(req.body);
-
-    // 응답하는 파일이 화면에 보여지기는 하지만 주소창은 바뀌지 않음
-    // -> 어색함
-    // res.sendFile(path.join(__dirname, 'views', 'login.html'));
-    // 이 자리에서 login.html을 응답하는 게 아니라
-    // /login 경로로 다시 요청
-    res.redirect('/login');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-
-app.listen(8888, ()=>{
-    console.log('Server is running on http://localhost:8888');
-});
+module.exports = app;
