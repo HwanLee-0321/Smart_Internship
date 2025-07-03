0816700e8037a127a2f3dfbcd2d0dd484e21f7f9
hwanlee-0321 <ghksdl413@naver.com>
2025-07-03 11:41:55 +0900

    로그인 기능을 위한 라우터 및 HTML 폼 추가

--- nodejs/ex04Post/app.js
+++ nodejs/ex04Post/app.js
@@ -4,6 +4,18 @@ const userRoutes = require('./routes/users');
 const session = require('express-session');
 const app = express();
 
+// middle-ware(미들웨어): 요청~응답 사이에 존재하는 기술들
+app.use(express.urlencoded({extended:true}));
+
+app.use(session({
+  secret: 'myScretKey',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 30
+  }
+}))
+
 app.use('/', indexRoutes);
 app.use('/users', userRoutes);
 
