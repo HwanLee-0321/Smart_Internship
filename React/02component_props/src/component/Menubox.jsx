155affae5e15642472b39a4721d2a3b5ee214be1
hwanlee-0321 <ghksdl413@naver.com>
2025-07-08 10:25:37 +0900

    commit

--- React/02component_props/src/component/Menubox.jsx
+++ React/02component_props/src/component/Menubox.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Menubox = (props) => {
+  // 부모(상위) 컴포터넌트로부터 받아올 데이터는 매개변수를 통해서 가져올 수 있다
+  // 넘어온 데이터 탑은 객체형태로 전달된다
+
   return (
     <div className="menu-card">
       <h3>
