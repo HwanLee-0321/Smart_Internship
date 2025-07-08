9c4f2230f16701f83b2a1d80c94665cc8639e30e
hwanlee-0321 <ghksdl413@naver.com>
2025-07-08 10:08:34 +0900

    App2 및 Menubox 컴포넌트 추가 및 main.jsx에서 StrictMode 제거

---
+++ React/02component_props/src/App2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Menubox from './component/Menubox'
+
+const App2 = () => {
+  return (
+    <div>
+        <Menubox name="아메리카노" price="3000"></Menubox>
+        <Menubox name="카페라떼" price="4000"></Menubox>
+        <Menubox name="아이스티" price="5000"></Menubox>
+    </div>
+  )
+}
+
+export default App2
---
+++ React/02component_props/src/component/Menubox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Menubox = (props) => {
+  console.log(props.price)
+  console.log(props.name)
+
+  return (
+    <div>
+      <div>
+            <h3>{props.name}</h3>
+            <p>{props.price}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Menubox
