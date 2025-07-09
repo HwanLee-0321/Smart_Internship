6c7b5c3d1576e1eff053afd7dc8989e5317262f8
hwanlee-0321 <ghksdl413@naver.com>
2025-07-09 10:18:15 +0900

    App2 컴포넌트 추가 및 좋아요 기능 구현

--- React/03usestate/src/App.jsx
+++ React/03usestate/src/App.jsx
@@ -3,7 +3,10 @@ import pic from '/img.gif'
 import './App.css'
 
 function App() {
+  // state 생성
   const [count, setCount] = useState(0)
+  const [likeEmoji, setLikeEmoji] = useState('♡')
+  const [likeNum, setLikeNum] = useState(0)
 
   const onIncrease = () => {
     setCount(count + 1);  
@@ -15,10 +18,25 @@ function App() {
     }
   }
 
+  const handleClick = () => {
+    if (likeEmoji === '♡') {
+      setLikeEmoji('♥');
+      setLikeNum(likeNum + 1);
+    } else {
+      setLikeEmoji('♡');
+      setLikeNum(likeNum - 1);
+    }
+  }
+
+
   return (
     <div> 
-      <img width="200px" src={pic} alt="" />
-      <h2>{count}</h2>
+      {/* <img width="200px" src={pic} alt="" /> */}
+      <p>
+        <span onClick={handleClick}>{likeEmoji}</span>
+        <span>{" "}좋아요 수: {likeNum}</span>
+      </p>
+      <h2>{count}</h2>  
       <button onClick={onIncrease}>+</button>
       <button onClick={onDecrease}>-</button>
     </div>
---
+++ React/03usestate/src/App2.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+function App2() {
+  // 내가 버튼을 클릭했을 때, 클릭한 숫자와 랜덤한 숫자를 비교
+  // 범위는 1~3
+
+  // state선언
+  // ranNum: 랜덤한 숫자 (1~3)
+  // inputNum: 내가 클릭한 숫자
+  const [ranNum, setRanNum] = useState(0)
+  const [inputNum, setInputNum] = useState(0)
+
+  // 함수 선언부
+  const btnCk = (choice) => {
+    // 1~3 범위의 랜덤한 숫자 생성
+    const randomNum = Math.floor(Math.random() * 3) + 1;
+    
+    // state 업데이트
+    setInputNum(choice);
+    setRanNum(randomNum);
+  }
+ 
+  return(
+    <div>
+      <h3>숫자 맞추기 게임</h3>
+      <div>
+        <button onClick={() => btnCk(1)}>1</button>
+        <button onClick={() => btnCk(2)}>2</button>
+        <button onClick={() => btnCk(3)}>3</button>
+      </div>
+
+      <div>
+        <p>내가 선택한 숫자: {inputNum}</p>
+        <p>랜덤 숫자: {ranNum}</p>
+        
+        {/* 처음에는 보이지 않다가 버튼 클릭 후 결과 표시 */}
+        {inputNum !== 0 && (
+            <h4>{inputNum === ranNum ? '정답입니다! 🎉' : '틀렸습니다! 😭'}</h4>
+        )}
+      </div>
+    </div>
+  )
+}
+export default App2
