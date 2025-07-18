f6516607905ab7c1371f785b449fb9034aaa6fbe
hwanlee-0321 <ghksdl413@naver.com>
2025-07-18 17:39:13 +0900

    refactor: 코드 정리 및 로그인 기능 개선

--- FullStack/react/src/components/Login.jsx
+++ FullStack/react/src/components/Login.jsx
@@ -3,30 +3,37 @@ import React, { useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  const idRef = useRef()
-  const pwRef = useRef()
-  const nav = useNavigate()
+	const idRef = useRef()
+	const pwRef = useRef()
+	const nav = useNavigate()
 
-  const tryLogin=()=>{
-    let id = idRef.current.value;
-    let pw = pwRef.current.value;
+	const tryLogin=()=>{
+		let id = idRef.current.value;
+		let pw = pwRef.current.value;
 
-    axios.post("http://localhost:8000/user/login", {
-      id : id,
-      pw : pw
-    })
-    .then((res)=>{
-      console.log(res.data.nick);
-    })
-  }
+		axios.post("http://localhost:8000/user/login", {
+		id : id,
+		pw : pw
+		})
+		.then((res)=>{
+		console.log(res.data.nick);
+		if(res.data == 0){
+			alert("로그인 실패!");
+		}else{
+			let nick = res.data.nick;
+			window.localStorage.setItem('nickname', nick);
+			nav('/home');
+		}
+		})
+	}
 
-  return (
-    <div>
-      ID : <input></input> <br />
-      PW : <input></input> <br />
-      <button onClick={tryLogin}>로그인 시도</button>
-    </div>
-  )
+	return (
+		<div>
+		ID : <input></input> <br />
+		PW : <input></input> <br />
+		<button onClick={tryLogin}>로그인 시도</button>
+		</div>
+	)
 }
 
 export default Login
