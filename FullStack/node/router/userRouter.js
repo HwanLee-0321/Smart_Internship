d79f8d3ee50e3ab741512b1c15280a87534a50e1
hwanlee-0321 <ghksdl413@naver.com>
2025-07-18 17:31:36 +0900

    feat: 로그인 기능 추가 및 서버와의 연동 구현

--- FullStack/node/router/userRouter.js
+++ FullStack/node/router/userRouter.js
@@ -1,3 +1,59 @@
+/*
+
+react-dom_client.js?v=e845238e:17987 Download the React DevTools for a better development experience: https://react.dev/link/react-devtools
+Login.jsx:11 Uncaught TypeError: Cannot read properties of undefined (reading 'value')
+    at tryLogin (Login.jsx:11:28)
+    at executeDispatch (react-dom_client.js?v=e845238e:11736:11)
+    at runWithFiberInDEV (react-dom_client.js?v=e845238e:1485:72)
+    at processDispatchQueue (react-dom_client.js?v=e845238e:11772:37)
+    at react-dom_client.js?v=e845238e:12182:11
+    at batchedUpdates$1 (react-dom_client.js?v=e845238e:2628:42)
+    at dispatchEventForPluginEventSystem (react-dom_client.js?v=e845238e:11877:9)
+    at dispatchEvent (react-dom_client.js?v=e845238e:14792:13)
+    at dispatchDiscreteEvent (react-dom_client.js?v=e845238e:14773:62)
+tryLogin @ Login.jsx:11
+executeDispatch @ react-dom_client.js?v=e845238e:11736
+runWithFiberInDEV @ react-dom_client.js?v=e845238e:1485
+processDispatchQueue @ react-dom_client.js?v=e845238e:11772
+(익명) @ react-dom_client.js?v=e845238e:12182
+batchedUpdates$1 @ react-dom_client.js?v=e845238e:2628
+dispatchEventForPluginEventSystem @ react-dom_client.js?v=e845238e:11877
+dispatchEvent @ react-dom_client.js?v=e845238e:14792
+dispatchDiscreteEvent @ react-dom_client.js?v=e845238e:14773
+<button>
+exports.jsxDEV @ react_jsx-dev-runtime.js?v=e845238e:250
+Login @ Login.jsx:24
+react-stack-bottom-frame @ react-dom_client.js?v=e845238e:17424
+renderWithHooks @ react-dom_client.js?v=e845238e:4206
+updateFunctionComponent @ react-dom_client.js?v=e845238e:6619
+beginWork @ react-dom_client.js?v=e845238e:7654
+runWithFiberInDEV @ react-dom_client.js?v=e845238e:1485
+performUnitOfWork @ react-dom_client.js?v=e845238e:10868
+workLoopSync @ react-dom_client.js?v=e845238e:10728
+renderRootSync @ react-dom_client.js?v=e845238e:10711
+performWorkOnRoot @ react-dom_client.js?v=e845238e:10330
+performWorkOnRootViaSchedulerTask @ react-dom_client.js?v=e845238e:11623
+performWorkUntilDeadline @ react-dom_client.js?v=e845238e:36
+<Login>
+exports.jsxDEV @ react_jsx-dev-runtime.js?v=e845238e:250
+App @ App.jsx:21
+react-stack-bottom-frame @ react-dom_client.js?v=e845238e:17424
+renderWithHooks @ react-dom_client.js?v=e845238e:4206
+updateFunctionComponent @ react-dom_client.js?v=e845238e:6619
+beginWork @ react-dom_client.js?v=e845238e:7654
+runWithFiberInDEV @ react-dom_client.js?v=e845238e:1485
+performUnitOfWork @ react-dom_client.js?v=e845238e:10868
+workLoopSync @ react-dom_client.js?v=e845238e:10728
+renderRootSync @ react-dom_client.js?v=e845238e:10711
+performWorkOnRoot @ react-dom_client.js?v=e845238e:10330
+performWorkOnRootViaSchedulerTask @ react-dom_client.js?v=e845238e:11623
+performWorkUntilDeadline @ react-dom_client.js?v=e845238e:36
+<App>
+exports.jsxDEV @ react_jsx-dev-runtime.js?v=e845238e:250
+(익명) @ main.jsx:10
+
+ */
+
 // 필요한 모듈 불러오기
 const express = require('express');
 const router  = express.Router();
@@ -25,6 +81,28 @@ router.post('/join', (req,res)=>{
             res.json({status : 'success'})
         } else {
             res.status(400).json({error:'회원가입 실패'})
+        }
+    })
+})
+
+// 로그인 처리
+router.post('/login', (req,res)=>{
+    console.log(req)
+    let {id, pw} = req.body;
+
+    // sql문 작성
+    let sql = 'select * from where id = ? AND pw =?';
+
+    // DB연동
+    conn.query(sql, [id, pw], (err,result)=>{
+        console.log(result);
+        if (result.length > 0){
+            console.log('succes')  
+            res.json({
+                nick:result[0].nick
+            })
+        } else {
+            res.json(0);
         }
     })
 })
--- FullStack/react/src/components/Login.jsx
+++ FullStack/react/src/components/Login.jsx
@@ -7,6 +7,19 @@ const Login = () => {
   const pwRef = useRef()
   const nav = useNavigate()
 
+  const tryLogin=()=>{
+    let id = idRef.current.value;
+    let pw = pwRef.current.value;
+
+    axios.post("http://localhost:8000/user/login", {
+      id : id,
+      pw : pw
+    })
+    .then((res)=>{
+      console.log(res.data.nick);
+    })
+  }
+
   return (
     <div>
       ID : <input></input> <br />
